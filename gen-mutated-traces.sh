a19d1208d1d443fd9ca0354ea731cc00e5a131ea
eabalestra <agusbalestra0@gmail.com>
2025-11-13 09:10:48 -0300

    Reduce timeout duration for mutant processing and improve code formatting

--- gen-mutated-traces.sh
+++ gen-mutated-traces.sh
@@ -25,20 +25,22 @@ echo ''
 
 cp_with_tests="$build_dir:$subject_sources/build/classes/java/test:$subject_sources/libs/*"
 
-TIMEOUT=7200
+TIMEOUT=3600
 echo '> Processing mutants'
-for dir in mutants/*/     # list directories in the form "/tmp/dirname/"
-do
-  echo '> Processing mutant: '$dir$target_file
-  echo '> Compiling mutant'
-  javac -cp $build_dir:$subject_sources/libs/* -g $dir$target_file -d $build_dir
-  echo '> Mutant compiled'
-  echo '' 
-  echo '> Generating traces with Chicory from mutant'
-  dir2=${dir%*/}
-  number=${dir2##*/}
-  timeout $TIMEOUT java -cp lib/daikon.jar:$cp_with_tests daikon.Chicory --output-dir=$mutants_dir --comparability-file=$setup_output_dir/$driver_base'Driver.decls-DynComp' --ppt-omit-pattern=$driver_base'.*' --ppt-omit-pattern='org.junit.*' --dtrace-file=$driver_base'Driver-m'$number'.dtrace.gz' testers.$driver_base'Driver' $mutants_dir/$driver_base'Driver-m'$number'-objects.xml'
+for dir in mutants/*/; do # list directories in the form "/tmp/dirname/"
+	echo '> Processing mutant: '$dir$target_file
+	echo '> Compiling mutant'
+	javac -cp $build_dir:$subject_sources/libs/* -g $dir$target_file -d $build_dir
+	echo '> Mutant compiled'
+	echo ''
+	echo '> Generating traces with Chicory from mutant'
+	dir2=${dir%*/}
+	number=${dir2##*/}
+	timeout $TIMEOUT java -cp lib/daikon.jar:$cp_with_tests daikon.Chicory --output-dir=$mutants_dir --comparability-file=$setup_output_dir/$driver_base'Driver.decls-DynComp' --ppt-omit-pattern=$driver_base'.*' --ppt-omit-pattern='org.junit.*' --dtrace-file=$driver_base'Driver-m'$number'.dtrace.gz' testers.$driver_base'Driver' $mutants_dir/$driver_base'Driver-m'$number'-objects.xml'
+	exit_code=$?
+	if [ $exit_code -eq 124 ]; then
+		echo "WARNING: Timeout for mutant $number after $TIMEOUT seconds"
+	fi
 done
 
 echo '> Done!'
-
